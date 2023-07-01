096d9652ed2ffe67ec17292ba278b972c6cfe75b
dries02 <44616127+dries02@users.noreply.github.com>
2023-07-01 15:42:40 +0200

    Logistic model bug fixes

--- logistic_model.py
+++ logistic_model.py
@@ -1,11 +1,16 @@
 import math
+import random
+
+RANDOM_SEED = 87
 
 
 class LogisticModel:
 	""" Represents a logistic model, defined by its model parameters (weights and bias).
-	The following reference was used for obtaining some more mathematical background on logistic regression:
-	Jurafsky, D. & Martin, J. (2023). Speech and Language Processing: Logistic Regression.
+	The following references were used for obtaining some more mathematical background (partial derivatives)
+	on logistic regression:
+	- Jurafsky, D. & Martin, J. (2023). Speech and Language Processing: Logistic Regression.
 	www.web.stanford.edu/~jurafsky/slp3/5.pdf
+	- Stanford University (2019). Section 3_soln. https://cs230.stanford.edu/fall2018/section_files/section3_soln.pdf
 	"""
 	def __init__(self, weights, bias):
 		"""
@@ -23,23 +28,30 @@ class LogisticModel:
 		logits = inner_product(self.__weights, image_vector) + self.__bias
 		return logistic_sigmoid(logits)
 
-	def __compute_gradients(self, data_point, predicted_probability):
+	def __compute_gradient(self, data_point, predicted_probability):
 		"""
-		Computes the gradients by comparing the predicted probability with the teacher output.
+		Computes the gradient by comparing the predicted probability with the teacher output.
+		We use Stochastic Gradient Descent.
 		"""
-		weight_gradients = []
+		weight_gradient = []
+		"""
+		Normally I do not put comments in text, but this is pretty remarkable:
+		the term 'p - y' (the difference between predicted probability and the true label) is derived from the partial 
+		derivative of the cost function with respect to the input to the output neuron. This is then useful for the 
+		chain rule.
+		"""
 		for i in range(len(self.__weights)):
-			weight_gradients.append((predicted_probability - data_point.get_label()) * data_point.get_vector()[i])
+			weight_gradient.append((predicted_probability - data_point.get_label()) * data_point.get_vector()[i])
 		bias_gradient = predicted_probability - data_point.get_label()
-		return weight_gradients, bias_gradient
+		return weight_gradient, bias_gradient
 
-	def __update_parameters(self, gradients, learning_rate):
+	def __update_parameters(self, gradient, learning_rate):
 		"""
-		Updates the model parameters based on the gradients and the learning rate.
+		Updates the model parameters based on the gradient and the learning rate.
 		"""
 		for i in range(len(self.__weights)):
-			self.__weights[i] -= learning_rate * gradients[0][i]
-		self.__bias -= learning_rate * gradients[1]
+			self.__weights[i] -= learning_rate * gradient[0][i]
+		self.__bias -= learning_rate * gradient[1]
 
 	def evaluate(self, evaluation_set, loss_function):
 		"""
@@ -53,7 +65,7 @@ class LogisticModel:
 		for data_point in evaluation_set:
 			predicted_probability = self.__predict(data_point.get_vector())
 			total_loss += loss_function(predicted_probability, data_point.get_label())
-			rounded_probability = round(predicted_probability)
+			rounded_probability = round(predicted_probability)  # binary decision with threshold 0.5
 			true_cases += rounded_probability == data_point.get_label()
 		return total_loss / len(evaluation_set), true_cases / len(evaluation_set)
 
@@ -66,20 +78,26 @@ class LogisticModel:
 		:param learning_rate: the speed with which the gradient descent is performed.
 		:param num_epochs: the number of epochs to train the model.
 		"""
-		# TODO maybe use patience rather than a number of epochs? Then see where it stagnates.
+		# fix the random seed for reproducibility
+		random.seed(RANDOM_SEED)
 		for epoch in range(num_epochs):
+			random.shuffle(training_set)
 			train_loss = 0
 			train_true_cases = 0
 			for data_point in training_set:
+				# Step 1: FP
 				predicted_probability = self.__predict(data_point.get_vector())
-				gradients = self.__compute_gradients(data_point, predicted_probability)
-				self.__update_parameters(gradients, learning_rate)
+				# Step 2: BP with SGD
+				gradient = self.__compute_gradient(data_point, predicted_probability)
+				# Step 3: Parameter updates
+				self.__update_parameters(gradient, learning_rate)
+				# Step 4:... profit? First compute some training statistics
 				train_loss += loss_function(predicted_probability, data_point.get_label())
 				rounded_probability = round(predicted_probability)
 				train_true_cases += rounded_probability == data_point.get_label()
 			val_loss, val_accuracy = self.evaluate(validation_set, loss_function)
 			print(f'Epoch {epoch + 1}:')
-			print(f'\ttrain loss = {train_loss}, train accuracy = {train_true_cases / len(training_set)}')
+			print(f'\ttrain loss = {train_loss / len(training_set)}, train accuracy = {train_true_cases / len(training_set)}')
 			print(f'\tval loss = {val_loss}, val accuracy = {val_accuracy}')
 			print()
 
@@ -111,6 +129,7 @@ def cross_entropy_loss(p, y, epsilon=10**-8):
 	:param epsilon: an epsilon used to deal with FPA.
 	:return: the log loss.
 	"""
+	# For FPE with the domain of log()
 	if p == 0 or p == 1:
 		p = abs(p - epsilon)
 	if 0 < p < 1:
